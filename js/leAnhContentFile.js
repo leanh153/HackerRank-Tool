5acb11613c62e1380cbb887e9c45382fae5b8ec9
leanh <anhle1531994@gmail.com>
2019-07-16 21:48:21 +0700

    add go to editor button not completely working

--- js/leAnhContentFile.js
+++ js/leAnhContentFile.js
@@ -1,20 +1,10 @@
 $(document).ready(function () {
-    // Inject to Website
+    // global variable
     let body = $('body');
-    $('head').append(`
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-    integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    `);
-
-
+    let isHackerRank = (/https:\/\/www.hackerrank.com\/(.)*/gi).test(document.URL);
+    let containEditor = body.find($('#codeshell-wrapper') ).length > 0;
+    // list of all tabs to be displayed
     let tabs = [];
-    class item {
-        constructor(title, link) {
-            this.title = title;
-            this.link = link;
-        }
-
-    }
 
     class tab {
         constructor(id, name, listItem, html) {
@@ -23,25 +13,26 @@ $(document).ready(function () {
             this.listItem = listItem;
             this.html = html;
         }
-        
-        createFromList() {
+
+        createTabsFromList() {
             if (this.listItem != null) {
                 let link = '<li><a id="tabName-' + this.id + '" href="#tabs-' + this.id + '">' + this.name + '</a></li>';
                 let content = createContent(listItem);
             }
         }
 
-        createFromHml() {
+        createTabsFromHtml() {
             if (this.html != null) {
                 let link = '<li class="nav-item"><a class="nav-link" id="tabName-' + this.id + '"  data-toggle="tab" role="tab" href="#tabs-' + this.id + '">' + this.name + '</a></li>';
                 let content = '<div class="tab-pane"  id="tabs-' + this.id + '" role="tabpanel">' + this.html + '</div>';
-                $('#myContent ul').append(link); 
+                $('#myContent ul').append(link);
                 $('#myContent .tab-content').append(content);
             }
         }
     }
 
-    function pushToolTab() {
+    // Adding default tool to tabs list
+    function pushSeparateToolToTabs() {
         let toolHtml = `<div class="col-12 form-group">
         <label for="text">Paste list number title here like 1.1.1 and 1.1.2</label>
         <textarea id="text" class="form-control"  rows="5"></textarea>
@@ -66,12 +57,191 @@ $(document).ready(function () {
         <br>
     </div>`;
 
-    let newTab = new tab(tabs.length + 1, "Separate Tool", null, toolHtml);
-    tabs.push(newTab);
+        let newTab = new tab(tabs.length + 1, "Separate Tool", null, toolHtml);
+        tabs.push(newTab);
     }
 
+    // Handle separate title number tool
+    function handleSeparateTool() {
+        let textInput = $('textarea#text');
+        let textSplit = '';
 
+        let outPutTextarea1 = $('textarea#text1');
+        let outPutTextarea2 = $('textarea#text2');
+        let outPutTextarea3 = $('textarea#text3');
+
+        let textToOut1 = '';
+        let textToOut2 = '';
+        let textToOut3 = '';
+
+
+
+        textInput.on('paste', function (e) {
+            navigator.clipboard.readText().then(clipText => {
+                textSplit = clipText.split('\n');
+                pasteTo1();
+                pasteTo2();
+                pasteTo3();
+
+            });
+
+
+        });
+
+        function pasteTo1() {
+            textSplit.forEach(element => {
+                if ((/^\d+\s/).test(element)) {
+                    textToOut1 += element + '\n';
+                }
+            });
+            outPutTextarea1.val(textToOut1);
+        }
+
+        function pasteTo2() {
+
+            textSplit.forEach(element => {
+
+                if ((/^\d+\.\d\s/).test(element)) {
+
+                    textToOut2 += element + '\n';
+                }
+            });
+            outPutTextarea2.val(textToOut2);
+        }
+
+        function pasteTo3() {
+
+            textSplit.forEach(element => {
+
+                if ((/^\d+\.\d+\.\d+\s/).test(element)) {
+
+                    textToOut3 += element + '\n';
+                }
+            });
+            outPutTextarea3.val(textToOut3);
+
+        }
+    }
+
+    // Handle HackerRank website
+    function handleHackerRank() {
+
+        // add go to hackerRank editor button
+        if (containEditor) {
+            let hackerRankEditorHref = "";
+            hackerRankEditorHref = document.URL;
+            let url = hackerRankEditorHref + "#codeshell-wrapper";
+            body.append(`
+                  <a id="goToEditor" href="`+ url + `" class="nav-item btn btn-success fas fa-code"></a>
+                   `);
+        }else{
+            $('#goToEditor').remove();
+        }
+
+        let fileInputName = $('#input_file_name');
+        $(window).scroll(function () {
+            let challengeCard = $('h4.challengecard-title').text();
+            // check if the url contain funix-lab that will treat the first regex and else
+            let title = challengeCard.replace((/(funix-lab)/gi).test(document.URL) ? /((\s{3,})|(\n))/gm
+                : /(Hard)?(Medium)?(Easy)?Max\s(\w*\D){7}/gm, "\n");
+            console.log("challengecard: " + challengeCard);
+            console.log("title separeated by new line: " + title);
+
+            fileInputName.text(title);
+            $('#numberLine').val(fileInputName.val().trim().split("\n").length);
+
+            console.log("challenge card text: " + challengeCard);
+            console.log("files Title: " + title);
+        });
+
+        fileInputName.mouseleave(function () {
+            $('#numberLine').val($('#input_file_name').val().trim().split("\n").length);
+        });
+
+        $("#btnSeparate").click(function (e) {
+            e.preventDefault();
+            let fileNames = $('#input_file_name').val().trim();
+            let editor = $('#editor');
+            let fileNameExtension = $("#input_file_Extension").val();
+            let startNumber = $("#input_start_number").val();
+            let listName = fileNames.split("\n");
+            let listTextAreaId = listName.slice(0);
+
+            if (fileNames.length === 0) {
+                alert("Please input file's name");
+                return;
+            }
+
+            for (let i = 0; i < listName.length; i++) {
+                let number = Number(startNumber) + i;
+                listName[i] = number + "_" + listName[i].replace(/\s+/g, "_") + fileNameExtension;
+            }
+
+            (function () {
+                let output = "";
+                for (let i = 0; i < listName.length; i++) {
+                    output += `<div class="row-count col-12 form-group"> 
+                    <label for="" class="title">` + listName[i] + `</label>
+                    <textarea id="editorText"  class="` +
+                        listTextAreaId[i].replace(/\s+/g, "") + ` form-control" rows = "5"  ></textarea >` +
+                        `</div >`;
+                }
+
+
+                editor.html("");
+                editor.append(output);
+                $('#numFileToSave').val(listName.length);
+                $("#btnSaveFileArea").show();
+            }).call(this);
+
+        });
+
+
+        $('#btnSaveFile').click(function () {
+            let rows = $('div.row-count');
+            console.log("type of rows: " + typeof rows);
+            let numberEmptyFile = 0;
+            let zip = new JSZip();
+            $.each($(rows), function (index, element) {
+                let fileName = $(element).find('.title').text();
+                let content = $(element).find('#editorText').val();
+                if (content.length > 0) {
+                    zip.file(fileName, content);
+                } else {
+                    numberEmptyFile++;
+                }
+            });
+
+
+            if (numberEmptyFile < rows.length && numberEmptyFile > 0) {
+                alert("There are " + numberEmptyFile + " empty, so those files would not to be zipped");
+            } else if (numberEmptyFile === rows.length) {
+                alert("Please input at least 1 file to zip");
+                return;
+            }
+
+
+            zip.generateAsync({ type: "blob" }).then(
+                function (blob) {
+                    // 1 generate the zip file
+                    let userName = $('span.username').text() || $('span.profile-username').text();
+                    saveAs(blob, userName.length === 0 ? "sourcecode.zip" : userName + ".zip"); // 2) trigger the download
+                },
+                function () {
+                }
+            );
+        });
+
+    }
+    // This append some ui to context web page
     function loadContent() {
+        // this link supplys some icon from fontawesome website
+        $('head').append(`
+       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        `);
+
+        // append default ui
         if (body.find($('#myContent')).length === 0) {
             let mainUI = `<div id="myContent" class="draggable ui-widget-content">
             <div class="btn btn-success fab fa-hackerrank" id="toggle"></div>
@@ -83,8 +253,8 @@ $(document).ready(function () {
         </div>`;
             body.prepend(mainUI);
         }
-
-        if ((document.URL).match(/https:\/\/www.hackerrank.com\/(.)*/gi)) {
+        // if url match hackerRank web site, adding this tab to tabs list
+        if (isHackerRank) {
             let hackerRankhtml = `<div class="col-12">
                 <textarea id="input_file_name" class="form-control" rows="6"
                     placeholder="Enter all your file's name here, separated by new line">
@@ -132,196 +302,46 @@ $(document).ready(function () {
         
             </div>
             <div class="col-12" id="editor"></div>
-            `;
             
+            `;
+
             let newTab = new tab(tabs.length + 1, "HackerRank", null, hackerRankhtml);
             tabs.push(newTab);
         }
-       pushToolTab();
+        // adding tool tab to tabs list which is tool help separating links number title
+        pushSeparateToolToTabs();
     }
 
-    function handleTool() {
-        let textInput = $('textarea#text');
-        let textSplit = '';
-
-        let outPutTextarea1 = $('textarea#text1');
-        let outPutTextarea2 = $('textarea#text2');
-        let outPutTextarea3 = $('textarea#text3');
-
-        let textToOut1 = '';
-        let textToOut2 = '';
-        let textToOut3 = '';
-
-
-
-        textInput.on('paste', function (e) {
-            navigator.clipboard.readText().then(clipText => {
-                textSplit = clipText.split('\n');
-                pasteTo1();
-                pasteTo2();
-                pasteTo3();
-
-            });
-
-
-        });
-
-        function pasteTo1() {
-            textSplit.forEach(element => {
-                if (element.match(/^\d+\s/)) {
-                    textToOut1 += element + '\n';
-                }
-            });
-            outPutTextarea1.val(textToOut1);
-        }
-
-        function pasteTo2() {
-
-            textSplit.forEach(element => {
-
-                if (element.match(/^\d+\.\d\s/)) {
-
-                    textToOut2 += element + '\n';
-                }
-            });
-            outPutTextarea2.val(textToOut2);
-        }
-
-        function pasteTo3() {
-
-            textSplit.forEach(element => {
-
-                if (element.match(/^\d+\.\d+\.\d+\s/)) {
-
-                    textToOut3 += element + '\n';
-                }
-            });
-            outPutTextarea3.val(textToOut3);
-
-        }
-    }
-
-    function handleHackerRank() {
-
-        let fileInputName = $('#input_file_name');
-
-        $(window).scroll(function () {
-            let challengeCard = $('h4.challengecard-title').text();
-            // check if the url contain funix-lab that will treat the first regex and else
-            let title = challengeCard.replace((document.URL).match(/(funix-lab)/gi) ? /((\s{3,})|(\n))/gm 
-            : /(Hard)?(Medium)?(Easy)?Max\s(\w*\D){7}/gm , "\n");
-            console.log("challengecard: " + challengeCard);
-            console.log("title separeated by new line: " + title);
-            
-            fileInputName.text(title);
-            $('#numberLine').val(fileInputName.val().trim().split("\n").length);
-
-            console.log("challenge card text: " + challengeCard);
-            console.log("files Title: " + title);
-        });
-
-        fileInputName.mouseleave(function () {
-            $('#numberLine').val($('#input_file_name').val().trim().split("\n").length);
-        });
-
-        $("#btnSeparate").click(function (e) {
-            e.preventDefault();
-            let fileNames = $('#input_file_name').val().trim();
-            let editor = $('#editor');
-            let fileNameExtension = $("#input_file_Extension").val();
-            let startNumber = $("#input_start_number").val();
-            let listName = fileNames.split("\n");
-            let listTextAreaId = listName.slice(0);
-            
-            if (fileNames.length === 0) {
-                alert("Please input file's name");
-                return;
-            }
-            
-            for (let i = 0; i < listName.length; i++) {
-                let number = Number(startNumber) + i;
-                listName[i] = number + "_" + listName[i].replace(/\s+/g, "_") + fileNameExtension;
-            }
-            
-            (function () {
-                let output = "";
-                for (let i = 0; i < listName.length; i++) {
-                    output += `<div class="row-count col-12 form-group"> 
-                    <label for="" class="title">` + listName[i] + `</label>
-                    <textarea id="editorText"  class="` + 
-                    listTextAreaId[i].replace(/\s+/g, "") + ` form-control" rows = "5"  ></textarea >` +
-                        `</div >`;
-                }
-
-
-                editor.html("");
-                editor.append(output);
-                $('#numFileToSave').val(listName.length);
-                $("#btnSaveFileArea").show();
-            }).call(this);
-            
-        });
-
-
-        $('#btnSaveFile').click(function () {
-            let rows = $('div.row-count');
-            console.log("type of rows: " + typeof rows);
-            let numberEmptyFile = 0;
-            let zip = new JSZip();
-            $.each($(rows), function (index, element) {
-                let fileName = $(element).find('.title').text();
-                let content = $(element).find('#editorText').val();
-                if (content.length > 0) {
-                    zip.file(fileName, content);
-                } else {
-                    numberEmptyFile++;
-                }
-            });
-
-
-            if (numberEmptyFile < rows.length && numberEmptyFile > 0) {
-                alert("There are " + numberEmptyFile + " empty, so those files would not to be zipped");
-            } else if (numberEmptyFile === rows.length) {
-                alert("Please input at least 1 file to zip");
-                return;
-            }
-
-
-            zip.generateAsync({ type: "blob" }).then(
-                function (blob) {
-                    // 1 generate the zip file
-                    let userName = $('span.username').text() || $('span.profile-username').text();
-                    saveAs(blob, userName.length === 0 ? "sourcecode.zip" : userName + ".zip"); // 2) trigger the download
-                },
-                function () {
-                }
-            );
-        });
-
-    }
-
+    // After pushing all tab to list tab, Display to context
     function updateUI() {
+        // for each tab in tabs display them
         tabs.forEach(tab => {
-            tab.createFromHml();
-            tab.createFromList();
+            tab.createTabsFromHtml();
+            tab.createTabsFromList();
         });
 
-        if((document.URL).match(/https:\/\/www.hackerrank.com\/(.)*/gi)){
+        if (isHackerRank) {
+            // handle on event on HackerRank website
             handleHackerRank();
+
         }
-
-        handleTool();
+        // handle separate tool event on website        
+        handleSeparateTool();
     }
-    
-    loadContent();
-    updateUI();
-   
-    
 
-    // Behavior Event
+    // this will check document and load neccessary content to memory
+    loadContent();
+    // this displays content to context
+    updateUI();
+
+
+
+    // Behavior Event, jqueryui 
     $('.draggable').draggable();
     $('#myContent li:first-child a').tab('show');
+    // make tab resizable
     $('.resizable').resizable();
+    // toggle when click on hackerRank button icon embeded on website
     $('#toggle').dblclick(function () {
         $('#tabs').toggle("slow/400/fast");
     });
